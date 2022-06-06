6e8fb63f2a497616da1766c46ebe5c0ce4559546
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-06-06 14:42:41 -0500

    Fix node version

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,18 +1,15 @@
-$nodeVer = "v18.1.0"
-$nodeVerName = "node-$nodeVer-darwin-x64"
+$nodeVer = "v18.3.0"
+$nodeVerName = "node-$nodeVer-win-x64"
 if (!(Test-Path "~/Documents/$nodeVerName")) {
-    # mkdir "~/Documents/$nodeVerName"
     cd ~/Downloads
     $WebClient = New-Object System.Net.WebClient
-    Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVer/$nodeVerName.tar.gz" -OutFile "./$nodeVerName.tar.gz"
-    tar -xf "$nodeVerName.tar.gz"
-    Start-Sleep -Milliseconds 500
-    mv "./$nodeVerName/" "~/Documents/"
-    rm "$nodeVerName.tar.gz"
+    Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVer/$nodeVerName.zip" -OutFile "./$nodeVerName.zip"
+    Expand-Archive -Path "$nodeVerName.zip" -DestinationPath "~/Documents/"
+    rm "$nodeVerName.zip"
 }
 
 $pathContent = [Environment]::GetEnvironmentVariable('path', 'User')
-$paths = "C:/Program Files/Java/jdk1.8.0_291/bin","~/Documents/$nodeVerName/bin","~/Documents/$nodeVerName/lib/node_modules/npm/bin"
+$paths = "C:/Program Files/Java/jdk1.8.0_291/bin","~/Documents/$nodeVerName"
 foreach ($path in $paths) {
     $path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
 
