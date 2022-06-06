b52aa46e909c5dca21f0697cf78f9972f128206e
raphtalia <59540801+raphtalia@users.noreply.github.com>
2022-06-05 23:59:39 -0500

    Update install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,2 +1,23 @@
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://nodejs.org/dist/v18.1.0/node-v18.1.0-darwin-x64.tar.gz", "%USERPROFILE%/Documents/node-v18.1.0-darwin-x64.tar.gz")
+$nodeVer = "v18.1.0"
+$nodeVerName = "node-$nodeVer-darwin-x64"
+if (!(Test-Path "~/Documents/$nodeVerName")) {
+    # mkdir "~/Documents/$nodeVerName"
+    cd ~/Downloads
+    $WebClient = New-Object System.Net.WebClient
+    Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVer/$nodeVerName.tar.gz" -OutFile "./$nodeVerName.tar.gz"
+    tar -xf "$nodeVerName.tar.gz"
+    Start-Sleep -Milliseconds 500
+    mv "./$nodeVerName/" "~/Documents/"
+    rm "$nodeVerName.tar.gz"
+}
+
+$pathContent = [Environment]::GetEnvironmentVariable('path', 'User')
+$paths = "C:/Program Files/Java/jdk1.8.0_291/bin","~/Documents/$nodeVerName/bin","~/Documents/$nodeVerName/lib/node_modules/npm/bin"
+foreach ($path in $paths) {
+    $path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
+
+    if ($pathContent -split ';'  -notcontains  $path) {
+        $pathContent += ";$path"
+        [Environment]::SetEnvironmentVariable("PATH", $pathContent, [EnvironmentVariableTarget]::User)
+    }
+}
